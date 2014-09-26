5e692a25c2e88e1ab2fb6bdfa1e17424b900755a
Jakub Jarosz <jakub.s.jarosz@gmail.com>
2014-09-26 06:58:55 +0100

    Initial commit, cli for circleclient

---
+++ circleclient/cli.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+circleclient.cli
+~~~~~~~~~~~~~~~~
+
+This module provides the CLI interface to cicleclient.
+"""
+
+import argparse
+import circleclient
+
+
+def main():
+    """Entry point for the application scipt."""
+    pass
+
+
+
+if __name__ == "__mian__":
+    main()
