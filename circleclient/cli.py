0672fc4f9f99db974564e2ea252ea86e5d018006
Jakub Jarosz <jakub.s.jarosz@gmail.com>
2014-09-29 07:49:34 +0100

    [WIP] cli for circleclient

--- circleclient/cli.py
+++ circleclient/cli.py
@@ -7,15 +7,27 @@ circleclient.cli
 This module provides the CLI interface to cicleclient.
 """
 
+
 import argparse
 import circleclient
 
 
 def main():
     """Entry point for the application scipt."""
-    pass
+    parser = argparse.ArgumentParser(prog="ccl")
+    subparsers = parser.add_subparsers(help="todo help")
+
+    parser_user = subparsers.add_parser(
+        'user', help='List information about the user.')
+
+    parser_projects = subparsers.add_parser(
+        'projects', help='List all projects.')
+
+    parser_cache = subparsers.add_parser(
+        'cache', help='Clear cache for given project')
+    parser_cache.add_argument('username', type=str, help='Username')
 
 
 
-if __name__ == "__mian__":
+if __name__ == "__main__":
     main()
